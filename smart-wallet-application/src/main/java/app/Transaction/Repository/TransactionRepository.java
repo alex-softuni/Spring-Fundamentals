626207dbe225868f5423fb1d7c925a62f91c2ff3
worldwidehandsomeJT <notorious.atanasow@gmail.com>
2025-02-28 13:45:19 +0200

    data access provided!

---
+++ smart-wallet-application/src/main/java/app/Transaction/Repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package app.Transaction.Repository;
+
+import app.Transaction.Model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+}
